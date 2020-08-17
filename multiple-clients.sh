9a85944f470fd81aeaf2393f148342e004e9a44f
Chad Jenison <cjenison@gmail.com>
2020-08-17 18:55:10 -0500

    things good

--- multiple-clients.sh
+++ multiple-clients.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 MEMBERS=50
-PROCESSES=20
-INTERVAL=5
+PROCESSES=50
+INTERVAL=1
 MODE="passthrough"
 
 for ((c=1; c<=$PROCESSES; c++))
 do
 echo $c
-(./f5_rest_api_pool_via_cli_script_tester.py --add --members $MEMBERS --bigip 192.168.72.245 --$MODE --user admin --poolName CHAD --poolipprefix 192.168.$c --interval $INTERVAL --password Elliott351@ &)
+sleep 2
+(./f5_rest_api_pool_via_cli_script_tester.py --add --members $MEMBERS --bigip 10.193.3.154 --$MODE --user admin --poolName CHAD --poolipprefix 192.168.$c --interval $INTERVAL --password admin &)
 done
