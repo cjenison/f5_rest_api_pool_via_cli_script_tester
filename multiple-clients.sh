958000e5098c304ccd2a504ad07ad8ee63a173f9
Chad Jenison <cjenison@gmail.com>
2020-08-17 12:39:20 -0500

    add shell script to call python script in parallel

---
+++ multiple-clients.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.6 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.7 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.8 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.9 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.10 --interval 1 --password Elliott351@ &
+
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.11 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.12 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.13 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.14 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.15 --interval 1 --password Elliott351@ &
+
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.16 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.17 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.18 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.19 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.20 --interval 1 --password Elliott351@ &
+
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.21 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.22 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.23 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.24 --interval 1 --password Elliott351@ &
+./f5_rest_api_pool_via_cli_script_tester.py --add --bigip 192.168.72.245 --defer --user admin --poolName CHAD --poolipprefix 192.168.25 --interval 1 --password Elliott351@ &
